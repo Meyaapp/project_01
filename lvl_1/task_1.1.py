95478e922fad4ab0b9b86b230ce1a7ddc245b7fe
Meyaapp <129536741+Meyaapp@users.noreply.github.com>
2023-06-02 14:43:14 +0300

    Добавили дз

---
+++ lvl_1/task_1.1.py
@@ -0,0 +1,18 @@
+# Задача 1.1.
+
+# Есть строка с перечислением песен
+
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+
+# Выведите на консоль с помощью индексации строки, последовательно: первый трек, последний, второй, второй с конца
+# Нельзя переопределять my_favorite_songs и запятая не должна выводиться.
+
+my_favorite_songs_lst = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+print(my_favorite_songs_lst[0:14])
+print(my_favorite_songs_lst[-14:])
+print(my_favorite_songs_lst[15:30])
+print(my_favorite_songs_lst[-26:-15])
+
+  
+
+
